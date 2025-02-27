989e2600ac41d0118feaa5cc95f0a94ccf99e6b2
Sameer Krishna Namdeo <149700104+sameernk@users.noreply.github.com>
2025-02-27 13:06:04 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,14 @@
+package q22546;
+import java.util.Scanner;
+
+public class CharCodeQuest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char magicalChar = scanner.next().charAt(0);
+    
+    int c=(int)magicalChar;
+        System.out.println(c);
+        scanner.close();
+    }
+
+}
